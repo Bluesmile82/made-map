e9ec0ac3b0a5db5b86a202d1d146194951a7bd93
Bluesmile82 <pursuance@gmail.com>
2014-12-05 15:36:48 +0100

    leyenda y selección

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@
 
 
 var 
- tileServer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+ tileServer = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
  //tileServer = 'Stamen.Watercolor',
    tileAttribution = 'Map data: <a href="http://www.openstreetmap.org/-28.3&layers=T">OSM</a>';
 
-var map = L.map("map").setView([20, 0], 2);
+var map = L.map("map",{bounceAtZoomLimits:false, worldCopyJump:true, zoomAnimationThreshold:7}).setView([20, 0], 2);
 
-  L.tileLayer(tileServer, {attribution: tileAttribution,  maxZoom: 13,minZoom:2}).addTo(map);
-var markers = L.markerClusterGroup();
-//var marker = [];
+  L.tileLayer(tileServer, {attribution: tileAttribution,  maxZoom: 4,minZoom:2}).addTo(map);
+var markers = L.markerClusterGroup({showCoverageOnHover:false});
+
+var markerMap = {};
+var popUpMap = {};
 d3.csv("datosGFMD.csv", function(data) {
 	for (var i = 1; i < data.length; i++) {
 
@@ -18,40 +20,55 @@ d3.csv("datosGFMD.csv", function(data) {
 
 	//	if (i===data.lenght-1)console.log("final");
 		if (!isNaN(parseFloat(data[i].lon))){var marker = L.marker([parseFloat(data[i].lon), parseFloat(data[i].lat)],{title:title});
-		marker.bindPopup(title);
-		printMarker(marker,title);
+		markerMap[i] = marker;
+		popUpMap[i] = markerMap[i].bindPopup(title);
+		printMarker(marker,title,i);
 		}else (console.log(i));
 
 			
 			markers.addLayer(marker);
-
+		// marker.openPopup();
 
 	};
 	map.addLayer(markers);
 
-	
+var seleccion = leyenda.selectAll("p");
+
+
+seleccion.on('mouseover', function() { 
+	d3.select(this).style("background-color", "lightblue");
+});
+seleccion.on('mouseout', function() { 
+	d3.select(this).style("background-color", "white");
 });
 
-var legend = L.control({position: 'bottomright'});
 
-legend.onAdd = function (map) {
+seleccion.on('click', function() { 
+	var m = markerMap[d3.select(this).attr("id")];
+	markers.zoomToShowLayer(m, function () { m.openPopup();});
 
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
+});
 
-    return div;
-};
 
-legend.addTo(map);
 
+
+});
+
+markers.on('click', function(m) {  console.log(m.layer.options.title)});
 
 
 var leyenda = d3.select("#leyenda");
-	function printMarker(mark,tit){
-	leyenda.append("p").text(tit).on("click",nothing);
+	
+
+	function printMarker(mark,tit,i){
+		
+	leyenda.append("p").attr("id",i).text(tit);
+
 	}
-function nothing(){};
+
+
+//.on("click",function(){ console.log("lala");});
+
 
 /*
 L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
