bf2396789f684d68ce45472960fe9904597a429a
Bluesmile82 <pursuance@gmail.com>
2014-12-06 15:37:08 +0100

    añadido info

--- js/script.js
+++ js/script.js
@@ -1,79 +1,129 @@
 
+var tileServer = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png'
 
-
-var 
- tileServer = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
- //tileServer = 'Stamen.Watercolor',
-   tileAttribution = 'Map data: <a href="http://www.openstreetmap.org/-28.3&layers=T">OSM</a>';
+var tileAttribution = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
 var map = L.map("map",{bounceAtZoomLimits:false, worldCopyJump:true, zoomAnimationThreshold:7}).setView([20, 0], 2);
 
-  L.tileLayer(tileServer, {attribution: tileAttribution,  maxZoom: 4,minZoom:2}).addTo(map);
+ 		  L.tileLayer(tileServer, {attribution: tileAttribution,  maxZoom: 4,minZoom:2, noWrap:true}).addTo(map);
+
 var markers = L.markerClusterGroup({showCoverageOnHover:false});
 
 var markerMap = {};
 var popUpMap = {};
-d3.csv("datosGFMD.csv", function(data) {
-	for (var i = 1; i < data.length; i++) {
+var webMap = {};
+
+
+
+
+d3.csv("datos.csv", function(data) {
+
+		for (var i = 1; i < data.length; i++) {
 
 			var title = data[i].Name;
+			var web = data[i].web; 
+			var country = data[i].country;
+			var city = data[i].city;
 
-	//	if (i===data.lenght-1)console.log("final");
-		if (!isNaN(parseFloat(data[i].lon))){var marker = L.marker([parseFloat(data[i].lon), parseFloat(data[i].lat)],{title:title});
-		markerMap[i] = marker;
-		popUpMap[i] = markerMap[i].bindPopup(title);
-		printMarker(marker,title,i);
-		}else (console.log(i));
 
-			
+				if (!isNaN(parseFloat(data[i].lon))){
+
+					var marker = L.marker([parseFloat(data[i].lon), parseFloat(data[i].lat)],{title:title},{alt:title});
+					
+					marker.id = i;
+					marker.web = web;
+					marker.country = country;
+					marker.city = city;
+					markerMap[i] = marker;
+					popUpMap[i] = markerMap[i].bindPopup(title);
+					printMarker(marker,i);
+					webMap[i] = web;
+
+
+				}else {(console.log(i));}
+
 			markers.addLayer(marker);
-		// marker.openPopup();
 
-	};
+
+			};
+
 	map.addLayer(markers);
+
 
 var seleccion = leyenda.selectAll("p");
 
 
 seleccion.on('mouseover', function() { 
+
 	d3.select(this).style("background-color", "lightblue");
+
+	d3.select(".info").transition().style("height","150px");
+
+	info(d3.select(this).attr("id"));
+
 });
+
+
 seleccion.on('mouseout', function() { 
+
 	d3.select(this).style("background-color", "white");
+
 });
 
 
 seleccion.on('click', function() { 
-	var m = markerMap[d3.select(this).attr("id")];
+
+	// map.zoomOut(3);
+
+    var i = d3.select(this).attr("id");
+	var m = markerMap[i];
+
 	markers.zoomToShowLayer(m, function () { m.openPopup();});
 
+    info(i);
+	
+
+});
+
 });
 
 
 
+markers.on('click', function(m) { 
+
+	info(m.layer.id);
 
 });
-
-markers.on('click', function(m) {  console.log(m.layer.options.title)});
-
 
 var leyenda = d3.select("#leyenda");
 	
 
-	function printMarker(mark,tit,i){
-		
-	leyenda.append("p").attr("id",i).text(tit);
-
-	}
 
 
-//.on("click",function(){ console.log("lala");});
+function printMarker(mark,i){
+	
+	leyenda.append("p").attr("id",i).text(mark.options.title);
 
 
-/*
-L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	minZoom: 1,
-	maxZoom: 16
-});
-*/
+}
+
+
+
+function info(id){
+
+	d3.select(".info").style("padding","20px").transition().style({height:"150px"});
+
+	// console.log("i"+i+"Nweb"+Nweb);
+
+	var muestraInfo = d3.select("#info").text("");
+	var web = markerMap[id].web;
+	var country = markerMap[id].country;
+	var city = markerMap[id].city;
+
+	muestraInfo.append("p").text(markerMap[id].options.title)
+			   .append("p").append("a").attr("href","http://" + web).text(web);
+	muestraInfo.append("p").text(country);
+	muestraInfo.append("p").text(city);
+
+;}
+
